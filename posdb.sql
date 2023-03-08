bde132f8c951ae4c0c0207193212472ff21b6741
zakaria belkercha <zakibelk11@gmail.com>
2023-03-08 22:38:33 +0100

    Add files via upload

---
+++ posdb.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 08 mars 2023 à 22:34
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 7.4.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `posdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `discounts`
+--
+
+CREATE TABLE `discounts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `percentage` decimal(5,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `discounts`
+--
+
+INSERT INTO `discounts` (`id`, `name`, `percentage`) VALUES
+(1, 'zaki', '47.00'),
+(3, 'mp', '70.00'),
+(4, 'zzz', '58.00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `tax` decimal(10,2) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `tax`, `image`) VALUES
+(1, 'samsung', '200.00', '10.00', 'sm.jpg'),
+(8, 'iphone 14 pro', '1000.00', '40.00', 'iphone-14-pro-max.png'),
+(10, 'Huawei P20 Pro', '900.00', '20.00', 'huawei-p20-pro.jpg'),
+(12, 'iphone 14 pro max', '7000.00', '100.00', 'CV_FORMATEUR.jpg');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `discounts`
+--
+ALTER TABLE `discounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `discounts`
+--
+ALTER TABLE `discounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
